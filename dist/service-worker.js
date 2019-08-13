dc9ec3b981a69851b8f2b2642d7547e6c5fcdeb2
Sofía Farfán <isfc@outlook.es>
2019-08-12 21:46:11 -0500

    fixed service worker

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
   "/precache-manifest.8bbd58167b8af52943ae2d3be91be0d3.js"
-=======
-  "/precache-manifest.8c10879723ff03c5c14125f42c0f0488.js"
->>>>>>> d6c04ea5b2ddc8cb2a505bc7326b497a4eb2aa54
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-material-dashboard"});
